7256cd4fc2600071d7aa08c440a71f2b0177b783
VladimirKoshovec <koshavladmir@gmail.com>
2023-06-01 00:41:54 +0300

    add_some_blocks

---
+++ src/components/UserLogo/UserLogo.jsx
@@ -0,0 +1,10 @@
+import scss from '../UserLogo/UserLogo.module.scss'
+
+const UserLogo = () => {
+    return (
+        <div className={ scss.userlogo }>
+        </div>
+    )
+};
+
+export default UserLogo;
--- src/components/Usercard/Usercard.jsx
+++ src/components/Usercard/Usercard.jsx
@@ -1,8 +1,18 @@
 import scss from '../Usercard/Usercard.module.scss';
+import { ReactComponent as CardLogo }from '../../images/logo.svg';
+import { ReactComponent as CardHeadsvg }from '../../images/picture2_1.svg';
+import UserLogo from '../UserLogo/UserLogo';
 
 const Usercard = () => {
     return (
         <div className={ scss.usercard }>
+            <div className={ scss.usercardheader }>
+                <CardLogo className={ scss.usercardlogo }/>
+                <CardHeadsvg className={ scss.usercardsvg }/>
+            </div>
+            <UserLogo/>  
+            <div className={ scss.usercardstripe }></div>
+
         </div>
     )
 };
