617127560abe22cc0d596f41a13ba0389c323586
VladimirKoshovec <koshavladmir@gmail.com>
2023-05-31 23:46:48 +0300

    start!

---
+++ src/components/Usercard/Usercard.jsx
@@ -0,0 +1,10 @@
+import scss from '../Usercard/Usercard.module.scss';
+
+const Usercard = () => {
+    return (
+        <div className={ scss.usercard }>
+        </div>
+    )
+};
+
+export default Usercard;
