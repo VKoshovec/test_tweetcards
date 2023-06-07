8bfdc2cb810a5722ebe6ba0fb365e43706cb9a2f
VladimirKoshovec <koshavladmir@gmail.com>
2023-06-07 23:58:27 +0300

    fix rout

--- src/components/Routing/Router.jsx
+++ src/components/Routing/Router.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
+import Loader from '../Loader/Loader';
+import { Suspense } from 'react';
 
 const Tweets = lazy(()=> import ('../../pages/users/Userspage'));
 const Home = lazy(()=> import ('../../pages/home/Home'));
@@ -7,10 +9,12 @@ const Home = lazy(()=> import ('../../pages/home/Home'));
 export const Router = () => {
 
     return(
+        <Suspense fallback={ <Loader/> }>
            <Routes>
               <Route path='/' element = { <Home /> } />
               <Route path='/tweets' element = { <Tweets /> } />
               <Route path='*' element = { <Home /> } />
            </Routes>
+        </Suspense>
     )
 };
--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { BrowserRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { Suspense } from "react";
 import Loader from './components/Loader/Loader';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter basename="/test_tweetcards">
-         <Suspense fallback={ <Loader/> }>
-           <App />
-         </Suspense>
-       </BrowserRouter>
+      <RouterProvider router={ router } />
     </Provider>
   // </React.StrictMode>
 );
